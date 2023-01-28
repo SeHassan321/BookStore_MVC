5bda604c31346d8535364bf227a6f7fdf1b217e7
Hassan <se.hassan321@gmail.com>
2023-01-28 13:44:52 +0200

    add-migration removeAuthorFromBook

---
+++ UISCOM/Migrations/20230128114126_removeAuthorFromBook.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UISCOM.Migrations
+{
+    public partial class removeAuthorFromBook : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Books_Authers_AutherId",
+                table: "Books");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Books_AutherId",
+                table: "Books");
+
+            migrationBuilder.DropColumn(
+                name: "AutherId",
+                table: "Books");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "AutherId",
+                table: "Books",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Books_AutherId",
+                table: "Books",
+                column: "AutherId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Books_Authers_AutherId",
+                table: "Books",
+                column: "AutherId",
+                principalTable: "Authers",
+                principalColumn: "Id");
+        }
+    }
+}

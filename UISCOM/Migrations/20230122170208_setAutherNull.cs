dc18519cc2ff40b71ddcf68ac0dc010085570922
Hassan <se.hassan321@gmail.com>
2023-01-28 13:31:09 +0200

    auther cant be null

---
+++ UISCOM/Migrations/20230122170208_setAutherNull.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UISCOM.Migrations
+{
+    public partial class setAutherNull : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Books_Authers_AutherId",
+                table: "Books");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "AutherId",
+                table: "Books",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Books_Authers_AutherId",
+                table: "Books",
+                column: "AutherId",
+                principalTable: "Authers",
+                principalColumn: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Books_Authers_AutherId",
+                table: "Books");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "AutherId",
+                table: "Books",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Books_Authers_AutherId",
+                table: "Books",
+                column: "AutherId",
+                principalTable: "Authers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
